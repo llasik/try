bdcc3b351464d9b62ae4c2f47916d664d9d1fd8c
unknown <do_not_enter@ukr.net>
2020-09-08 19:52:28 +0300

    sign up

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const loadform = async () => {
-  const html = await (await fetch('https://alina-osyka.github.io/test/resources/form.html')).text()
+  const html = await (await fetch('https://https://github.com/llasik/try.git/resources/form.html')).text()
   document.body
     .appendChild(document.createElement('main'))
       .innerHTML = html
-  const [login, password, avatar, submit, picture] = ['login', 'password', 'avatar', 'submit', 'picture'].map((id) => document.getElementById(id))
+  const [login, password, avatar, submit, picture] = ['login', 'password', 'avatar', 'submit', 'picture']
+  .map((id) => document.getElementById(id))
+  
   login.onchange = function(event) {
     fetch(`http://localhost:3000/users?login=${event.target.value}`)
       .then((response) => response.json())
@@ -11,15 +13,23 @@ const loadform = async () => {
         if (response.length === 0) {
           event.target.style.color = 'green'
           password.disabled = false
+          localStorage.setItem('user', event.target.value)
         } else {
           event.target.style.color = 'red'
           password.disabled = true
         }
       })
   }
+  
   password.onchange = function(event) {
-
+    if (event.target.length >= 8 && event.target.match (/[\dA-Z]+)/g) {
+      avatar.disabled = false
+      submit.disabled = false
+    var hash = Sha256.hash(event.target.value)
+    localStorage.setItem('pass', hash)
+    }
   }
+  
   submit.onclick = function(event) {
     fetch('http://localhost:3000/users', {
       method: 'POST',
@@ -28,11 +38,12 @@ const loadform = async () => {
       },
         body: JSON.stringify({
           login: login.value,
-          password: password.value,
+          password: localStorage.getItem('pass'),
           avatar: picture.src
         })
     }).then((response) => console.log(response.status))
   }
+  
   avatar.onchange = function(event) {
     if (event.target.files[0].type.indexOf('image') !== 0) {
       console.warn('Invalid file type')
@@ -45,4 +56,5 @@ const loadform = async () => {
     }
   }
 }
+
 loadform()
